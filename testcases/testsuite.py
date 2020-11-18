a49df55ef2406ce26c22ee21cec684046dac51d5
Pankaj <pankaj@arcgate.com>
2020-11-18 18:58:13 +0530

    uadded testsuite file

--- testcases/testsuite.py
+++ testcases/testsuite.py
@@ -1,30 +1,20 @@
 import unittest
-import xmlrunner
-
 from testcases.test001_login_test import LoginTests
-from testcases.global_catalog.test_global_catalog001 import GlobalCatalog1
-from testcases.LayOut_Floor_Plan.test_layout_floor_plan import Floor_Plan
+from testcases.LayOut_Floor_Plan.test_layout_floor_plan import FloorPlanTest
+from testcases.global_catalog.test_global_catalog001 import GlobalCatalogTest
+
+# Get all tests from the test classes
+
+tc3 = unittest.TestLoader().loadTestsFromTestCase(LoginTests)
+tc1 = unittest.TestLoader().loadTestsFromTestCase(FloorPlanTest)
+tc2 = unittest.TestLoader().loadTestsFromTestCase(GlobalCatalogTest)
 
 
-def create_suite(classes):
-    suite = unittest.TestSuite()
-    for _class in classes:
-        _object = _class()
-        for function_name in dir(_object):
-            if function_name.lower().startswith("test"):
-                suite.addTest(_class(function_name))
-    return suite
+# Create a test suite combining all test classes
+smokeTest = unittest.TestSuite([tc3, tc1, tc2])
 
 
-def run_unit_tests():
-    runner = xmlrunner.XMLTestRunner(verbosity=2)
-    classes = [
-        LoginTests,
-        GlobalCatalog1,
-        Floor_Plan
-    ]
-    runner.run(create_suite(classes))
+unittest.TextTestRunner(verbosity=2).run(smokeTest)
 
 
-if __name__ == "__main__":
-    run_unit_tests()
+
